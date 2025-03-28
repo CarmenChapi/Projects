1334a5122c6946b5f09efaf4973d5aa4db9e7d3a
CarmenChapi <mariachaparro58@gmail.com>
2025-03-28 20:11:43 +0000

    add linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import { AiFillGithub } from "react-icons/ai";
 import { TfiEmail } from "react-icons/tfi";
 import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="Footer">
+      
+      <a
+        alt="linkedin"
+        href="https://www.linkedin.com/in/maria-del-carmen-chaparro-g%C3%B3mez-403655358/"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Contact with me by linkedin"
+      >
+          <FaLinkedin />
+          </a>
       <a
         alt="email link"
         href="mailto:mariachaparro58@gmail.com?subject=Hi Carmen"
