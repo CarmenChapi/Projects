5d4c8f26052c7b16837462b6b8fdf998313cb1cb
CarmenChapi <mariachaparro58@gmail.com>
2026-07-15 16:48:59 +0100

    this commmit will updated google analytic check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import "./App.css";
+import ReactGA from "react-ga4";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import ProjectsList from "./components/ProjectsList";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import WorksList from "./components/WorksList";
+
+const GA_MEASUREMENT_ID = "G-LJPFQT48Z3";
+
+if (import.meta.env.PROD) {
+  ReactGA.initialize(GA_MEASUREMENT_ID, {
+    gtagOptions: {
+      send_page_view: false,
+    },
+  });
+}
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!import.meta.env.PROD) {
+      return;
+    }
+
+    ReactGA.send({
+      hitType: "pageview",
+      page: `${location.pathname}${location.search}`,
+      title: document.title,
+    });
+  }, [location]);
+
+  return null;
+}
 
 function NotFound() {
   return (
@@ -26,22 +56,21 @@ function NotFound() {
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <div className="mainContainer">
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<AboutMe />} />
-            <Route path="/projects" element={<ProjectsList />} />
-            <Route path="/works" element={<WorksList />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+    <BrowserRouter>
+      <Analytics />
+      <Header />
+      <div className="mainContainer">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/projects" element={<ProjectsList />} />
+          <Route path="/works" element={<WorksList />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
 
-        <Footer />
-      </BrowserRouter>
-    </>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
